dbf611eade5f5e8d62d483dcf63a32d9b27b24ca
senadev42 <senadevv42@gmail.com>
2023-07-07 17:14:08 +0300

    Homepage skeleton

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,10 @@
+import Hero from "../components/Hero";
+
+const HomeScreen: React.FC = () => {
+  return (
+    <div className=" mx-4 flex flex-col items-center">
+      <Hero />
+    </div>
+  );
+};
+export default HomeScreen;
