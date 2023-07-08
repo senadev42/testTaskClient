f1211bdbfd4c700c58566771c1de37d8b9c9b93c
senadev42 <senadevv42@gmail.com>
2023-07-08 09:48:09 +0300

    added store and provider to main

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+//router imports
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
+
+//redux
+import store from "./store";
+import { Provider } from "react-redux";
 
 import HomePage from "./pages/HomePage.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
@@ -25,7 +30,9 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
