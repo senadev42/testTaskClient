76f073fbec88b1114c497511ed454b70bfa2a697
senadev42 <senadevv42@gmail.com>
2023-07-08 14:10:17 +0300

    responsive page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -3,9 +3,9 @@ import RegisterForm from "../components/RegisterForm";
 
 const LoginPage: React.FC = () => {
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-col sm:flex-row w-full">
       {/* teal rectangle */}
-      <div className="bg-teal-400 w-1/6 h-screen"></div>
+      <div className="bg-teal-400 hidden sm:flex sm:w-1/6 h-screen "></div>
 
       <RegisterForm />
     </div>
