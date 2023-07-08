35234d06c055d522dae11e8e27692bd73a273f1c
senadev42 <senadevv42@gmail.com>
2023-07-08 09:36:25 +0300

    linting

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+//import React, { useState } from "react";
 
 //import Link
 import { Link } from "react-router-dom";
