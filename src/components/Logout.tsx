57de104b85e703e8744affb203ace7a0d58547d7
senadev42 <senadevv42@gmail.com>
2023-07-08 19:42:24 +0300

    dedicated logout component

---
+++ src/components/Logout.tsx
@@ -0,0 +1,32 @@
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import { logout } from "../slices/authSlice";
+
+import { toast } from "react-toastify";
+
+const Logout: React.FC = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [logoutApiCall, { isLoading }] = useLogoutMutation();
+
+  const handleLogout = async () => {
+    console.log("button clicked");
+    try {
+      // send log out api call
+      await logoutApiCall();
+      // clear local storage
+      dispatch(logout());
+      //back to homepage
+      navigate("/");
+      //tell user
+      toast.success("You're Logged Out");
+    } catch (err: any) {
+      toast.error(err?.data?.message || err.error);
+    }
+  };
+
+  return <button onClick={handleLogout}>Logout</button>;
+};
+
+export default Logout;
