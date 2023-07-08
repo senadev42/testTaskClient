b059e296bf41f83d8e7ca6da2a151404b28454f6
senadev42 <senadevv42@gmail.com>
2023-07-08 09:48:31 +0300

    set up store, general api and auth slices

---
+++ src/slices/authSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface AuthSliceState {
+    userInfo: {
+        _id: string;
+        name: string;
+        email: string;
+        token: string;
+    } | null;
+}
+
+const initialState: AuthSliceState = {
+    userInfo: localStorage.getItem('userInfo')
+        ? JSON.parse(localStorage.getItem('userInfo') as string)
+        : null,
+};
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        setCredentials: (state, action: PayloadAction<AuthSliceState['userInfo']>) => {
+            state.userInfo = action.payload;
+            localStorage.setItem('userInfo', JSON.stringify(action.payload));
+        },
+        logout: (state) => {
+            state.userInfo = null;
+            localStorage.removeItem('userInfo');
+        },
+    },
+});
+
+export const { setCredentials, logout } = authSlice.actions;
+
+export default authSlice.reducer;
