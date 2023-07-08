edd04e326153b17c81780e43a8a562fee76054b1
senadev42 <senadevv42@gmail.com>
2023-07-08 10:43:18 +0300

    toast container

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import { Outlet } from "react-router-dom";
 
 import "./App.css";
 import Header from "./components/Header";
-import HomePage from "./pages/HomePage";
+
+//toast
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
       <Header />
+      <ToastContainer />
       <Outlet />
     </>
   );
