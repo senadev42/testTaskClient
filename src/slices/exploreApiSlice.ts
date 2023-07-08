3f13d02f6aa9528bb378fa4b3b6ec8bbc80d84d5
senadev42 <senadevv42@gmail.com>
2023-07-08 21:59:31 +0300

    the explore api slice

---
+++ src/slices/exploreApiSlice.ts
@@ -0,0 +1,19 @@
+import { apiSlice } from './apiSlice';
+
+
+//The explore url
+const EXPLORE_URL = '/api/explore';
+
+
+export const exploreApiSlice: any = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        nearbyCities: builder.mutation({
+            query: (data) => ({
+                url: `${EXPLORE_URL}/nearby-cities`,
+                method: 'POST',
+                body: data,
+            }),
+        })
+    }),
+});
+export const { nearbyCities } = exploreApiSlice;
