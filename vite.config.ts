5d3be95c20426db15329739be0a4058a11490980
senadev42 <senadevv42@gmail.com>
2023-07-10 11:47:30 +0300

    configured for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log(process.env.VITE_ENV);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,10 +12,14 @@ export default defineConfig({
   server: {
     port: 4000,
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: `${
+          process.env.VITE_ENV == "development"
+            ? "http://localhost:5000"
+            : "https://testtask-server.onrender.com/"
+        }`,
         changeOrigin: true,
       },
     },
   },
-})
+});
