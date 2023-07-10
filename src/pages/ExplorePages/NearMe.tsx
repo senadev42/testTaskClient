ba474487bca179d71a69352d15b1735b2e32f94b
senadev42 <senadevv42@gmail.com>
2023-07-10 10:21:26 +0300

    added loader to get geo location

--- src/pages/ExplorePages/NearMe.tsx
+++ src/pages/ExplorePages/NearMe.tsx
@@ -8,6 +8,7 @@ import { useNearbyCitiesMutation } from "../../slices/exploreApiSlice";
 import { toast } from "react-toastify";
 
 import { RootState } from "../../store";
+import Loader from "../../components/Loader";
 
 type CityNearMe = {
   cityname: string;
@@ -24,13 +25,20 @@ const NearMe = () => {
 
   //Local State
   const [cityData, setCityData] = useState<CityNearMe[]>();
+  const [distance, setDistance] = useState(20);
 
   //fetch handling
   const [position, setPosition] = useState<string | null>(null);
-  const [geolocationAvailable, setGeolocationAvailable] = useState(false);
+  const [geolocationAvailable, setGeolocationAvailable] = useState(false); //what does this serve?
   const [locationReject, setIsLocationReject] = useState(false);
 
-  const fetchCities = async () => {
+  //input handler
+  const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDistance(parseInt(event.target.value));
+  };
+
+  //Utility Functions
+  const getLocationFromBrowser = async () => {
     //location permission asking
     if ("geolocation" in navigator) {
       await navigator.geolocation.getCurrentPosition(
@@ -60,34 +68,40 @@ const NearMe = () => {
 
   useEffect(() => {
     if (position) {
-      apiCall()
-        .then(() => {
-          toast.success("Fetched Cities Near You");
-        })
-        .catch((err) => {
-          toast.error(err.message);
-        });
+      apiCall();
     }
   }, [position]);
 
   const apiCall = async () => {
     try {
-      //DEMO Test
-      //let coords = "+32.109333+34.855499";
-      //let _id = "23456";
+      //no need to make the call if data already exists on the page
+      if (cityData) return;
 
+      //1. Set Body Variables
       let _id = userInfo._id;
       let coords = position;
 
-      console.log(coords);
-      console.log(position);
-
+      //2. Send off request
       const res = await nearbyCities({ _id, coords }).unwrap();
 
-      setCityData(res);
+      //3. Checking for and throwing errors
+      if (res === undefined) {
+        throw new Error("No cities found");
+      }
+      if (res.message) {
+        throw new Error(res.message);
+      }
 
-      console.log(res);
+      // 4. if it clears error handling, it can load
+      setCityData(res);
+      toast.success("Fetched Cities Near You");
     } catch (err: any) {
+      //Rate limited error
+      if (err.message.includes("rate limit")) {
+        toast.error("Rate limited exceeded. Try again later.");
+      } else {
+        toast.error("Sorry something went wrong.");
+      }
       console.log(err.message);
     }
   };
@@ -96,88 +110,83 @@ const NearMe = () => {
     <div className="flex flex-row  h-full">
       <div className=" w-full bg-white p-8 mr-4 text-lg">
         <div className="flex flex-col items-center justify-center">
-          {/* Current Position */}
-          {/* <div className="bg-red-100 p-10">
-            {position ? (
-              <p> debug: Curr_position: {position}</p>
-            ) : (
-              <p>Retrieving current position...</p>
-            )}
-          </div> */}
-          {/* What's near me button */}
-          {/* TODO: slider to decide distance */}
-          <div className=" flex flex-col items-center">
-            <button
-              className={`mt-4 border-2 border-teal-200 px-4 py-2 hover:bg-teal-200`}
-              onClick={fetchCities}
-            >
-              What's Near Me?
-            </button>
-            {locationReject ? (
-              <p className="text-xs mt-2 text-center bg-red-100">
-                You need to turn on location to see what's near you.
-              </p>
-            ) : (
-              " "
-            )}
+          <div>
+            <div className=" flex flex-col items-center shadow-sm">
+              <button
+                className={`mt-4 border-2 border-teal-200 px-4 py-2 hover:bg-teal-200 text-center 
+                flex items-center justify-center`}
+                onClick={getLocationFromBrowser}
+              >
+                {isLoading ? <Loader /> : " What's Near Me?"}
+              </button>
+              {locationReject ? (
+                <p className="text-xs mt-2 text-center bg-red-100">
+                  You need to turn on location to see what's near you.
+                </p>
+              ) : (
+                " "
+              )}
+            </div>
+            <div>
+              {/* slider to filter cityData by city.distance*/}
+              {cityData && (
+                <div className="my-4">
+                  <input
+                    className="slider w-full"
+                    type="range"
+                    min="0"
+                    max="60"
+                    value={distance}
+                    onChange={handleSliderChange}
+                  />
+                  <div className="text-center">Distance</div>
+                </div>
+              )}
+            </div>
           </div>
 
           {/* Cities Near You */}
           {/* expects back a list of cities */}
           {cityData && (
-            <div className="mt-4 px-6">
-              <table className="table-auto w-full">
-                <thead className="bg-gray-200">
-                  <tr>
-                    <th className="px-4 py-2 text-left font-semibold text-gray-700">
-                      <button>City Name</button>
-                    </th>
-                    <th className="px-4 py-2 text-left font-semibold text-gray-700">
-                      Distance
-                    </th>
-                    <th className="px-4 py-2 text-left font-semibold text-gray-700">
-                      Region
-                    </th>
-                    <th className="px-4 py-2 text-left font-semibold text-gray-700">
-                      Population
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="bg-white">
-                  {cityData.map((city) => (
-                    <tr key={city.cityname} className="hover:bg-gray-100">
-                      <td className="px-4 py-2 border border-gray-300">
-                        {city.cityname}
-                      </td>
-                      <td className="px-4 py-2 border border-gray-300">
-                        {city.distance} km
-                      </td>
-                      <td className="px-4 py-2 border border-gray-300">
-                        {city.region}
-                      </td>
-                      <td className="px-4 py-2 border border-gray-300">
-                        {city.population}
-                      </td>
+            <div className="mt-4 px-6 mb-12">
+              <div className="flex flex-col items-center">
+                <table className="table-auto w-full text-sm sm:text-md">
+                  <thead className="bg-gray-200 text-zinc-700 border-b-2 border-black ">
+                    <tr>
+                      <th className="px-4 py-2 text-left font-semibold  border ">
+                        <button className="w-full">City Name</button>
+                      </th>
+                      <th className="px-4 py-2 text-left font-semibold  border ">
+                        Distance
+                      </th>
+                      <th className="px-4 py-2 text-left font-semibold  border w-[12rem] ">
+                        Region
+                      </th>
+                      <th className="px-4 py-2 text-left font-semibold  border">
+                        Population
+                      </th>
                     </tr>
-                  ))}
-                  {cityData.map((city) => (
-                    <tr key={city.cityname} className="hover:bg-gray-100">
-                      <td className="px-4 py-2 border border-gray-300">
-                        {city.cityname}
-                      </td>
-                      <td className="px-4 py-2 border border-gray-300">
-                        {city.distance} km
-                      </td>
-                      <td className="px-4 py-2 border border-gray-300">
-                        {city.region}
-                      </td>
-                      <td className="px-4 py-2 border border-gray-300">
-                        {city.population}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody className="bg-white">
+                    {cityData &&
+                      cityData
+                        .filter((city) => city.distance <= distance)
+                        .map((city) => (
+                          <tr
+                            key={city.cityname}
+                            className="hover:bg-gray-100 border border-gray-300"
+                          >
+                            <td className="px-4 py-2 ">{city.cityname}</td>
+                            <td className="px-4 py-2">{city.distance} KM</td>
+                            <td className="px-4 py-2 ">{city.region}</td>
+                            <td className="px-4 py-2">
+                              {city.population == 0 ? "~" : city.population}
+                            </td>
+                          </tr>
+                        ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
           )}
 
